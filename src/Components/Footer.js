c2370263c8425fe8bbf54af250444633b826faed
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-06-29 12:16:36 +0200

    update components

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,12 @@
+import {Link} from "./index";
+
 export default function Footer() {
-    return (<footer className="mt-40 text-center text-sm font-mono text-gray-400">
+    return (<footer className="mt-40 mb-4 text-center text-sm font-mono text-gray-400">
         <img src="https://solana.com/branding/horizontal/logo-horizontal-gradient-dark.png"
              className="w-40 mx-auto my-2" alt="Solana logo"/>
         <small>
-            <code>BUIDLed by <a href="https://streamflow.finance" className="text-gray-300" rel="noopener noreferrer"
-                                target="_blank">StreamFlow</a><br/>
-                during <a href="https://solana.com/solanaszn" target="_blank" className="text-gray-300"
-                   rel="noopener noreferrer">SOLANASZN</a></code></small>
+            <Link url="https://streamflow.finance" title="StreamFlow" hideIcon={true}/>
+            @ <Link url="https://solana.com/solanaszn" title="SolanaSZN" hideIcon={true}/>
+        </small>
     </footer>)
 }
--- src/Components/Link.js
+++ src/Components/Link.js
@@ -1,12 +1,13 @@
 import {ExternalLinkIcon} from "@heroicons/react/outline";
 
-export default function Link(props: { url: string, title?: string, className?: string }) {
-    const {url, title, className} = props;
+export default function Link(props: { url: string, title?: string, className?: string, hideIcon?: boolean}) {
+    const {url, title, className, hideIcon} = props;
+    const icon = hideIcon || <sup><ExternalLinkIcon className="w-3 h-3 inline"/></sup>
 
     return (
         <strong className={"text-gray-300 hover:text-white " + className}>
             <a href={url} target="_blank" rel="noopener noreferrer">
-                {title || url} <sup><ExternalLinkIcon className="w-3 h-3 inline"/></sup>
+                {title || url} {icon}
             </a>
         </strong>
     )
