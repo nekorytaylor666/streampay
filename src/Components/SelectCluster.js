b11bdfcf0eec21438564144678aea8fd9de74a91
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-08-27 01:07:36 +0200

    Removed the "cluster". Updated HTML tag id and name.

--- src/Components/SelectCluster.js
+++ src/Components/SelectCluster.js
@@ -7,16 +7,16 @@ export default function SelectCluster() {
     const [cluster, setCluster] = useNetworkStore(networkStore)
     return (
         <select
-            id="token"
-            name="token"
+            id="cluster"
+            name="cluster"
             className="mt-1 text-white bg-gray-800 border-primary block w-full border-black rounded-md focus:ring-secondary focus:border-secondary"
             defaultValue={cluster}
             onChange={e => setCluster(e.target.value)}
         >
-            <option value={CLUSTER_MAINNET}>{CLUSTER_MAINNET} cluster</option>
-            <option value={CLUSTER_DEVNET}>{CLUSTER_DEVNET} cluster</option>
-            <option value={CLUSTER_TESTNET}>{CLUSTER_TESTNET} cluster</option>
-            <option value={CLUSTER_LOCAL}>{CLUSTER_LOCAL} cluster</option>
+            <option value={CLUSTER_MAINNET} disabled={true}>{CLUSTER_MAINNET} — soon™</option>
+            <option value={CLUSTER_DEVNET}>{CLUSTER_DEVNET}</option>
+            <option value={CLUSTER_TESTNET}>{CLUSTER_TESTNET}</option>
+            <option value={CLUSTER_LOCAL}>{CLUSTER_LOCAL}</option>
         </select>
     )
 }
