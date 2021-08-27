dfba25b2ab2942d51dc23b7fba38a27c6288cd06
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-08-27 21:38:05 +0200

    lazy load logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import {Link} from "./index";
 export default function Footer() {
     return (<footer className="mt-40 mb-4 text-center text-sm font-mono text-gray-400">
         <img src="https://solana.com/branding/horizontal/logo-horizontal-gradient-dark.png"
-             className="w-40 mx-auto my-2" alt="Solana logo"/>
+             className="w-40 mx-auto my-2" alt="Solana logo" loading="lazy"/>
         <small>
             <Link url="https://streamflow.finance" title="StreamFlow" hideIcon={true}/>
             @ <Link url="https://solana.com/solanaszn" title="SolanaSZN" hideIcon={true}/>
