fd0bc29628065864d765d52a9b5916d90e1e3680
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-09-08 00:14:38 +0200

    remove duplicate select

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {ToastContainer} from "react-toastify";
 import {Main} from "./Pages";
-import {Footer, Logo, SelectCluster} from "./Components";
+import {Footer, Logo} from "./Components";
 import logo from './logo.png'
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -9,7 +9,6 @@ function App() {
         <div>
             <div className={"mx-auto bg-blend-darken px-4 my-4"}>
                 <Logo src={logo}/>
-                <div className="sm:absolute top-0 right-0 sm:p-4"><SelectCluster/></div>
                 <Main/>
             </div>
             <ToastContainer hideProgressBar position="bottom-left" limit={3}/>
