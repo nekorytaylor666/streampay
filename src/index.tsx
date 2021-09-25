fbbf7e031bd238df9a98391711cfdb1e6111ae20
Aleksandar <aleksandar@kushim.vc>
2021-09-25 11:05:22 +0200

    Adds prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 // import reportWebVitals from './reportWebVitals';
-import {FormProvider} from "./Contexts/FormContext";
+import { FormProvider } from "./Contexts/FormContext";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <FormProvider>
-            <App/>
-        </FormProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <FormProvider>
+      <App />
+    </FormProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
