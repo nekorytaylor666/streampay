f9e46a6ff0596d85c6a8f30836fb49409ec99b0d
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-11-04 22:52:41 +0100

    Deleted comments

--- src/stores/StreamsStore.ts
+++ src/stores/StreamsStore.ts
@@ -2,12 +2,8 @@ import { TokenStreamData } from "@streamflow/timelock/dist/layout";
 
 const useStreamStore = (set: Function, get: Function) => ({
   streams: {} as { [s: string]: TokenStreamData },
-  // streamingMints: [] as string[], // use in future
   addStream: (id: string, stream: TokenStreamData) =>
     set({ streams: { ...get().streams, [id]: stream } }),
-  // addStreamingMint: (mint: string) => { //todo in future concat!!!
-  //   set({ streamingMints: [mint] });
-  // },
   deleteStream: (id: string) => {
     const streams = { ...get().streams };
     delete streams[id];
