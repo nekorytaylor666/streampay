aabba06d275f2505308290f74e0e98e359e8aa34
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-11-07 23:57:34 +0000

    Regular O instead of SOL sign

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ const Logo: FC<LogoProps> = ({ src }) => (
   <div className="text-white mt-1">
     <h1 className="text-xl sm:text-2xl">
       <img src={src} alt="StreamFlow Finance logo" className="w-10 mr-1 inline-block" />
-      Stream<strong>Fl◎w</strong>
+      Stream<strong>Flow</strong>
     </h1>
   </div>
 );
