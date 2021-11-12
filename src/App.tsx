2d7cfad42a51c1b664cd77a5dc853cf778d39160
Dijana Pavlovic <dijana.pavlovic@kindredgroup.com>
2021-11-12 11:49:58 +0100

    SF-73: Try with 404.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
-import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { useEffect } from "react";
+
+import { BrowserRouter as Router, Switch, Route, Redirect, useHistory } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 import { Footer, Header, Nav } from "./components";
 import routes from "./RoutesConfig";
 
-const App = () => (
-  <div className="min-h-screen flex flex-col">
-    <div className="bg-blend-darken flex-grow px-3.5 sm:px-5">
-      <Router>
-        <Header />
-        <Nav classes="block lg:hidden" />
-        <Switch>
-          {routes.map(({ path, exact, redirect, Component }) => (
-            <Route
-              key={path}
-              path={path}
-              exact={exact}
-              render={() => (redirect ? <Redirect to={redirect} /> : <Component />)}
-            />
-          ))}
-        </Switch>
-      </Router>
+const App = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const path = localStorage.getItem("path");
+    if (path) {
+      console.log("path", path);
+      localStorage.removeItem("path");
+      history.push(path);
+    }
+  }, [history]);
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="bg-blend-darken flex-grow px-3.5 sm:px-5">
+        <Router>
+          <Header />
+          <Nav classes="block lg:hidden" />
+          <Switch>
+            {routes.map(({ path, exact, redirect, Component }) => (
+              <Route
+                key={path}
+                path={path}
+                exact={exact}
+                render={() => (redirect ? <Redirect to={redirect} /> : <Component />)}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+      <ToastContainer hideProgressBar position="bottom-left" limit={5} />
+      <Footer />
     </div>
-    <ToastContainer hideProgressBar position="bottom-left" limit={5} />
-    <Footer />
-  </div>
-);
+  );
+};
 
 export default App;
