5e737ced65eb149042de1128c4d1b970e2964ff1
Dijana Pavlovic <dijana.pavlovic@kindredgroup.com>
2021-11-23 00:11:30 +0100

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 import { Footer, Header, Nav } from "./components";
+import { Page404 } from "./pages";
 import routes from "./RoutesConfig";
 
 const App = () => (
   <div className="min-h-screen flex flex-col">
-    <div className="bg-blend-darken flex-grow px-3.5 sm:px-5">
+    <div className="bg-blend-darken flex-grow px-3.5 sm:px-5 flex flex-col">
       <Header />
       <Nav classes="block lg:hidden" />
       <Switch>
@@ -19,6 +20,7 @@ const App = () => (
             render={() => (redirect ? <Redirect to={redirect} /> : <Component />)}
           />
         ))}
+        <Route component={Page404} />
       </Switch>
     </div>
     <ToastContainer hideProgressBar position="bottom-left" limit={5} />
