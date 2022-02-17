14d7770f8cd7b77995f977cb7996491f75da4faa
imprfekt <5047969+imprfekt@users.noreply.github.com>
2022-02-17 12:46:53 +0100

    Make Link an actual link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -13,8 +13,8 @@ const Link: FC<LinkProps> = ({ url, title, Icon, classes }) => (
   <span className={cx("text-gray-200", classes)}>
     {Icon ? (
       <>
-        {title || url}
         <a href={url} target="_blank" rel="noopener noreferrer">
+         {title || url}
           <sup>
             <Icon className="w-3 h-3 inline hover:opacity-60" />
           </sup>
