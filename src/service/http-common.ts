5565d5ae4ba60353f6c7fe536404a5b5dfa5e0b5
Nemanja <Nemanja>
2022-04-07 12:42:15 +0200

    remove unused code

--- src/service/http-common.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://staging-api.internal-streamflow.com/v1/api",
-  headers: {
-    "Content-type": "application/json",
-  },
-});
