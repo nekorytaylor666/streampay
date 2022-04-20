393e69a99631eb06829ec184fb1251bc435d6e54
Akhmetov Toktar <toktar.akhmetov@softline.com>
2022-04-20 17:56:55 +0600

    feat(react-query): installed and initialized react query

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { Cluster } from "@streamflow/stream";
 import cx from "classnames";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+import useStore, { StoreType } from "./stores";
 import { trackPageView } from "./utils/marketing_helpers";
 import { Footer, Header, Banner, Nav, VerticalNav, Curtain } from "./components";
 import { Page404 } from "./pages";
 import routes from "./router/RoutesConfig";
 import PrivateRoute from "./router/PrivateRoute";
-import useStore, { StoreType } from "./stores";
 
 const storeGetter = ({ connection, wallet, cluster, loading }: StoreType) => ({
   connection: connection(),
@@ -20,6 +21,7 @@ const storeGetter = ({ connection, wallet, cluster, loading }: StoreType) => ({
   isMainnet: cluster === Cluster.Mainnet,
   loading,
 });
+const queryClient = new QueryClient();
 
 const App: FC = () => {
   const history = useHistory();
@@ -35,58 +37,60 @@ const App: FC = () => {
   }, [history, cluster]);
 
   return (
-    <div className={cx("min-h-screen flex flex-col", isMainnet ? "bg-main" : "bg-sandbox")}>
-      <Curtain visible={loading} />
-      {!isMainnet && (
-        <Banner
-          title="This is devnet (sandbox) environment!"
-          classes="top-0 left-0 w-full"
-        ></Banner>
-      )}
-      <div className="flex-grow flex flex-col bg-dark">
-        <Header toggleVerticalNav={toggleVerticalNav} isVerticalNavOpened={isVerticalNavOpened} />
-        {wallet?.connected && <Nav classes="hidden sm:block lg:hidden mb-2 mt-4" />}
-        <div className={`flex ${!wallet?.connected && "justify-center"}`}>
-          {wallet?.connected && isVerticalNavOpened && (
-            <VerticalNav
-              routes={routes.slice(3)}
-              classes="flex sm:hidden fixed top-18 left-0 z-50 w-screen h-screen"
-              onClick={toggleVerticalNav}
-            />
-          )}
-          {wallet?.connected && (
-            <VerticalNav routes={routes.slice(5)} classes="hidden sm:flex w-60 xl:w-64" />
-          )}
-          <Switch>
-            {routes.map(({ path, exact, Component, isPrivate, redirect }) =>
-              isPrivate ? (
-                <PrivateRoute
-                  key={path}
-                  exact={exact}
-                  path={path}
-                  isAuthenticated={wallet?.connected || false}
-                >
-                  {redirect ? <Redirect to={redirect} /> : <Component />}
-                </PrivateRoute>
-              ) : (
-                <Route key={path} path={path} exact={exact}>
-                  {redirect ? <Redirect to={redirect} /> : <Component />}
-                </Route>
-              )
+    <QueryClientProvider client={queryClient}>
+      <div className={cx("min-h-screen flex flex-col", isMainnet ? "bg-main" : "bg-sandbox")}>
+        <Curtain visible={loading} />
+        {!isMainnet && (
+          <Banner
+            title="This is devnet (sandbox) environment!"
+            classes="top-0 left-0 w-full"
+          ></Banner>
+        )}
+        <div className="flex-grow flex flex-col bg-dark">
+          <Header toggleVerticalNav={toggleVerticalNav} isVerticalNavOpened={isVerticalNavOpened} />
+          {wallet?.connected && <Nav classes="hidden sm:block lg:hidden mb-2 mt-4" />}
+          <div className={`flex ${!wallet?.connected && "justify-center"}`}>
+            {wallet?.connected && isVerticalNavOpened && (
+              <VerticalNav
+                routes={routes.slice(3)}
+                classes="flex sm:hidden fixed top-18 left-0 z-50 w-screen h-screen"
+                onClick={toggleVerticalNav}
+              />
             )}
-            <Route component={Page404} />
-          </Switch>
+            {wallet?.connected && (
+              <VerticalNav routes={routes.slice(5)} classes="hidden sm:flex w-60 xl:w-64" />
+            )}
+            <Switch>
+              {routes.map(({ path, exact, Component, isPrivate, redirect }) =>
+                isPrivate ? (
+                  <PrivateRoute
+                    key={path}
+                    exact={exact}
+                    path={path}
+                    isAuthenticated={wallet?.connected || false}
+                  >
+                    {redirect ? <Redirect to={redirect} /> : <Component />}
+                  </PrivateRoute>
+                ) : (
+                  <Route key={path} path={path} exact={exact}>
+                    {redirect ? <Redirect to={redirect} /> : <Component />}
+                  </Route>
+                )
+              )}
+              <Route component={Page404} />
+            </Switch>
+          </div>
         </div>
+        <ToastContainer
+          hideProgressBar
+          position="top-right"
+          limit={2}
+          className="sm:w-96 sm:mt-2 sm:r-6"
+          toastClassName="bg-gray-dark rounded-lg drop-shadow-lg"
+        />
+        <Footer />
       </div>
-      <ToastContainer
-        hideProgressBar
-        position="top-right"
-        limit={2}
-        className="sm:w-96 sm:mt-2 sm:r-6"
-        toastClassName="bg-gray-dark rounded-lg drop-shadow-lg"
-      />
-      <Footer />
-    </div>
+    </QueryClientProvider>
   );
 };
 
